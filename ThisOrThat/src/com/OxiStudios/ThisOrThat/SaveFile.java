326223abf194bf69dbe6fa2e6de406e99bad7ff2
Noah Butler <butlernc@uwec.edu>
2014-02-19 14:40:35 -0600

    save file

--- ThisOrThat/src/com/OxiStudios/ThisOrThat/SaveFile.java
+++ ThisOrThat/src/com/OxiStudios/ThisOrThat/SaveFile.java
@@ -1,11 +1,15 @@
 package com.OxiStudios.ThisOrThat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+
 public class SaveFile {
 	
 	//highest score
 	public float highScore;
 	
-	//amount of time played in the game
+	//amount of time played in the game in seconds
 	public float playTime;
 	
 	//highest amount that the player got in a row
@@ -17,22 +21,41 @@ public class SaveFile {
 	//closest time to losing
 	public float closestTime;
 	
-	//logest game they have played
+	//longest game they have played
 	public float longestGame;
 	
 	//total amount of games played
 	public float gamesPlayed;
+	
+	FileHandle file;
+	
+	String readFile;
+	
+	Array<Character> chars;
 	
 	public SaveFile() {
 		load();
 	}
 	
 	private void load() {
-		
+		if(!Gdx.files.external("ThisOrThat/save.save").exists()) {
+			this.file = Gdx.files.external("ThisOrThat/save.save");
+			save();
+		}else{
+			file = Gdx.files.external("ThisOrThat/save.save");
+			readFile = file.readString();
+			for(int i = 0; i < 10; ++i) {
+				chars.add(readFile.subSequence(i, i).charAt(0));
+			}
+		}
 	}
 	
 	public void save() {
-		//save the game file
+		//save the game files
+		file.writeString(Float.toString(gamesPlayed) + "\n", false);
+		file.writeString(Float.toString(longestGame) + "\n", true);
+		file.writeString(Float.toString(closestTime) + "\n", true);
+		file.writeString(Float.toString(fastestReponse) + "\n", true);
 	}
 
 }
